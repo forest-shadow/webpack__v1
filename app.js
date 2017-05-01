1719cd84b90c89633ac153f580de3efac80a1fed
forest-shadow <mamasay@yandex.ru>
2017-05-01 20:24:42 +0300

    add react deps; add react loader; create basic users react app

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const api         = require( './api.js' ),
       $           = require( 'jquery' ),
       fxRates     = require( './fx_rates.js' ),
-      welcomeUser = require('./welcomeUser.coffee');
+      welcomeUser = require('./welcomeUser.coffee'),
+      ReactApp    = require( './react/ReactApp.js');
 
 let users = api.getUsers();
 
 fxRates( 'USD', data => console.log( data ) );
 
-$.each(users, (index, user) => $(document.body).append( `<p>${index} - name ${user.name} age ${user.age}</p>`)
-);
+// $.each(users, (index, user) => $(document.body).append( `<p>${index} - name ${user.name} age ${user.age}</p>`)
+// );
 
 welcomeUser('Jones');
---
+++ react/MainComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import UsersComponent from './UsersComponent';
+
+export default class MainComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+          <h1>Users</h1>
+          <UsersComponent/>
+      </div>
+    );
+  }
+}
